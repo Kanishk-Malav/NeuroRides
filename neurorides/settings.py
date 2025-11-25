e0cda3b1752b399e0718757a7fa02c68692a9482
Kanishk malav <kanishkmalav@Kanishks-MacBook-Air.local>
2025-11-25 10:23:42 +0530

    Fix: settings.py for enviro var

--- neurorides/settings.py
+++ neurorides/settings.py
@@ -20,7 +20,7 @@ env = environ.Env(
 environ.Env.read_env(BASE_DIR / '.env')
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = os.environ.get("SECRET_KEY", env("SECRET_KEY", default="dev-fallback-secret-key"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
