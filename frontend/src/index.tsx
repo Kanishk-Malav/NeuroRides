55e0281041b06d25dfb81129eaef8ba03d20be54
Kanishk malav <kanishkmalav@Kanishks-MacBook-Air.local>
2025-12-02 09:50:51 +0530

    Fix: changes in frontend files

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+import { Provider } from 'react-redux';
+import { store } from './store'; // yahi jo pehle bhi use kar rahe the
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      {/* Router already inside App.tsx, so no BrowserRouter here */}
+      <App />
+    </Provider>
   </React.StrictMode>
 );
